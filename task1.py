05b8b2d18591201b40ef6a39e6dcd382f6f139c2
OlegAndreev <andreev008@mail.ru>
2023-09-08 14:56:58 +0300

    task1 is ready

--- task1.py
+++ task1.py
@@ -1,1 +1,7 @@
-print("hello world")
+# Найдите сумму цифр трехзначного числа n.
+# Результат сохраните в перменную res.
+number = int(input("Введите трехзначное число: "))
+while number < 100 or number > 999:
+    number = int(input("Число не 3-значное! Попробуйте еще раз: "))
+res = number % 10 + number // 100 + (number // 10) % 10
+print(f"Сумма чисел числа {number} равна {res}")
