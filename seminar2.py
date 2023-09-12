f191137936ec1f6ce9ff58b499c59c74d641ffa7
OlegAndreev <andreev008@mail.ru>
2023-09-12 11:36:13 +0300

    add new task, fibonacci

--- seminar2.py
+++ seminar2.py
@@ -1,8 +1,31 @@
 # факториал числа
-n = int(input("Введите n: "))
-factorial = 1
-help_number = 1
-while help_number <= n:
-    factorial *= help_number
-    help_number += 1
-print(factorial)
+# n = int(input("Введите n: "))
+# factorial = 1
+# help_number = 1
+# '''
+# n = input("vvedite n")
+# while not n.isdigit():
+#     print("вы ввели не число")   # проверка на то что мы вводим число
+#     n = input("vvedite n")
+# n = int(n)
+# '''
+# while help_number <= n:
+#     factorial *= help_number
+#     help_number += 1
+# print(factorial)
+
+#порядковый номер числа фибоначчи
+fib1 = 0
+fib2 = 1
+fib = 1
+count = 2
+fib_number = int(input('введите число '))
+while fib < fib_number:
+    fib = fib1 + fib2
+    fib1 = fib2
+    fib2 = fib
+    count += 1
+if fib == fib_number:
+    print(count)
+else:
+    print('-1')
