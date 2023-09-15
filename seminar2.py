bac0cfbd2ec8faf7a6d5ac3ebd05d0169dd95407
OlegAndreev <andreev008@mail.ru>
2023-09-15 09:57:43 +0300

    task bout watermels

--- seminar2.py
+++ seminar2.py
@@ -15,17 +15,48 @@
 # print(factorial)
 
 #порядковый номер числа фибоначчи
-fib1 = 0
-fib2 = 1
-fib = 1
-count = 2
-fib_number = int(input('введите число '))
-while fib < fib_number:
-    fib = fib1 + fib2
-    fib1 = fib2
-    fib2 = fib
-    count += 1
-if fib == fib_number:
-    print(count)
-else:
-    print('-1')
+# fib1 = 0
+# fib2 = 1
+# fib = 1
+# count = 2
+# fib_number = int(input('введите число '))
+# while fib < fib_number:
+#     fib = fib1 + fib2
+#     fib1 = fib2
+#     fib2 = fib
+#     count += 1
+# if fib == fib_number:
+#     print(count)
+# else:
+#     print('-1')
+
+# задача с оттепелью
+# num = int(input('количество дней: '))
+# count = 0
+# max_count = 0
+# for t in range(num):
+#     temp = int(input('введите температуру '))
+#     if temp > 0:
+#         count += 1
+#     else:
+#         count = 0
+#     if count > max_count:
+#         max_count = count
+# print(max_count)
+
+# задача с арбузами
+# import random
+# #watermel = int(input('колво арбузов '))
+# max_kg = 3
+# min_kg = 12
+# num = random.randint(5, 10)
+# print(num)
+# for i in range(num):
+#     kg = random.randint(3, 12)
+#     print(kg)
+#     if kg > max_kg:
+#         max_kg = kg
+#     elif kg < min_kg:
+#         min_kg = kg
+# print(min_kg, max_kg)
+    
